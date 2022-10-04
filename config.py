91601f0278c60a2f97969605299bc40004644342
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2022-10-04 22:21:25 +1100

    Defined configuration variables.

---
+++ config.py
@@ -0,0 +1,29 @@
+BASE_DIR = {
+    "deck": "decks/",
+    "font": "fonts/"
+}
+
+FONT = {
+    "small": {
+        "line_spacing": 0.75,
+        "text_font": BASE_DIR["font"] + "Bookerly-Regular-12.pcf",
+        "text_length_range": range(225, 375),
+        "text_wrap": 48
+    },
+    "medium": {
+        "line_spacing": 0.75,
+        "text_font": BASE_DIR["font"] + "Bookerly-Regular-16.pcf",
+        "text_length_range": range(75, 225),
+        "text_wrap": 35
+    },
+    "large": {
+        "line_spacing": 0.75,  # formerly 1.0
+        "text_font": BASE_DIR["font"] + "Bookerly-Regular-20.pcf",
+        "text_length_range": range(0, 75),
+        "text_wrap": 30
+    }
+}
+
+POSITION_VECTOR = {
+    "center": (0.5, 0.5)
+}
