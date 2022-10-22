c0d9a8a17c6fc360239b36f3d2bb868aff4ac68b
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2022-10-23 05:33:20 +1100

    Defined global constants relevant to general configuration.

--- config.py
+++ config.py
@@ -1,24 +1,33 @@
+"""
+Definitions of global constants pertaining to general configuration.
+
+Written by techn0mancr.
+"""
+
+# Custom library imports
+from helper import path_join
+
 BASE_DIR = {
-    "deck": "decks/",
-    "font": "fonts/"
+    "deck": path_join("decks"),
+    "font": path_join("fonts")
 }
 
 FONT = {
     "small": {
         "line_spacing": 0.75,
-        "text_font": BASE_DIR["font"] + "Bookerly-Regular-12.pcf",
+        "text_font": path_join(BASE_DIR["font"], "Bookerly-Regular-12.pcf"),
         "text_length_range": range(225, 375),
         "text_wrap": 48
     },
     "medium": {
         "line_spacing": 0.75,
-        "text_font": BASE_DIR["font"] + "Bookerly-Regular-16.pcf",
+        "text_font": path_join(BASE_DIR["font"], "Bookerly-Regular-16.pcf"),
         "text_length_range": range(75, 225),
         "text_wrap": 35
     },
     "large": {
         "line_spacing": 0.75,  # formerly 1.0
-        "text_font": BASE_DIR["font"] + "Bookerly-Regular-20.pcf",
+        "text_font": path_join(BASE_DIR["font"], "Bookerly-Regular-20.pcf"),
         "text_length_range": range(0, 75),
         "text_wrap": 30
     }
